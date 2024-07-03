440ea588168449088013089ad0fde499aca9d789
Vitu <vitorcsharp@hotmail.com>
2024-07-03 15:33:54 -0300

    Adding packages and creating a connection between hangfire and sqlserver

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Hangfire;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Adicionando o banco que estamos usando ao hangfire
+//Seria tipo informar ao hangfire que estamos usando o banco mysqlserver e a connectionString dele esta na variavel connectionString
+builder.Services.AddHangfire( (sp, config) =>
+{
+    //Pegar a string de conexao do nosso banco sqlserver e adicionar ao hangfire
+    var connectionString = sp.GetRequiredService<IConfiguration>().GetConnectionString("DefaultConnection");
+    config.UseSqlServerStorage(connectionString);
+});
+
+//Abrir o servidor do hangfire
+builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
